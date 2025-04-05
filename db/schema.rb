4e763521d4e96fd4b1b6dba4f9ecc69057698646
suds56 <zeke_rohrer@taylor.edu>
2025-04-04 22:50:33 -0400

    added cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_08_230257) do
+ActiveRecord::Schema[7.0].define(version: 2025_04_05_024454) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
