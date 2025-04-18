2283d5d50c48809bafa41c3f9038fd536e9e86de
suds56 <zeke_rohrer@taylor.edu>
2025-04-17 21:35:24 -0400

    Updated Cart Partial

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,6 +28,13 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            :cart,
+            partial: 'layouts/cart',
+            locals: {cart: @cart}
+          )
+        end
         format.html { redirect_to store_index_url}
         format.json { render :show, status: :created, location: @line_item }
       else
