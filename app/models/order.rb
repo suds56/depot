ed908620d38a41c631cb67eef1368a42ce99a5f4
suds56 <zeke_rohrer@taylor.edu>
2025-04-25 16:49:38 -0400

    Fixed keys bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order < ApplicationRecord
   }
 
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.key
+  validates :pay_type, inclusion: pay_types.keys
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
