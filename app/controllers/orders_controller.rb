6ac838929ddafb59f6edd019796e04d728b115f0
suds56 <zeke_rohrer@taylor.edu>
2025-05-13 18:22:59 -0400

    Fixed some email bugs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice:
           'Thank you for your order.' }
         format.json { render :show, status: :created,
@@ -81,5 +81,16 @@ class OrdersController < ApplicationController
       if @cart.line_items.empty?
         redirect_to store_index_url, notice: 'Your cart is empty'
       end
+    end
+
+    def pay_type_params
+      if order_params[:pay_type] == "Credit card"
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase order"
+        params.require(:order).permit(:po_number)
+      end
+      {}
     end
 end
--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="payment"
 export default class extends Controller {
-  static targets = ["selection", "additionialFields"]
+  static targets = ["selection", "additionalFields"]
 
   initialize() {
     console.log("Hello")
