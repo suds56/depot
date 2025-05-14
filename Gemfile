1c83b8940f775606e763301e2f5b9c619af62a02
suds56 <zeke_rohrer@taylor.edu>
2025-05-13 21:12:41 -0400

    Added emailed receiving and storing

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "bootsnap", require: false
 
 # Use Sass to process CSS
 # gem "sassc-rails"
+gem "letter_opener"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
+  # config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
     address: "ezekielrohrer1@gmail.com",
